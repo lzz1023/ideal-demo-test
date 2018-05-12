5cbaf8e0fd9c490fa9183e8929aed25bccec7fb1
lzz7626 <lzz@7626>
2018-05-12 13:21:33 +0800

    补充Test

--- src/main/java/com/ideal/Test.java
+++ src/main/java/com/ideal/Test.java
@@ -10,6 +10,7 @@ public class Test {
     public static void main(String[] args) {
 
         int line = Integer.parseInt(args[0]);
+        int lines = Integer.parseInt(args[2]);
         for(int i=0 ;i<line;i++){
             System.out.println("===================第"+(i+1)+"行=====================");
         }
