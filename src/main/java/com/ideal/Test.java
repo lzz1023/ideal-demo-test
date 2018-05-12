d6f28f4d49847a4a4fcb0c0bc755938b6ed22532
lzz7626 <lzz@7626>
2018-05-12 12:16:23 +0800

    初始化项目

---
+++ src/main/java/com/ideal/Test.java
@@ -0,0 +1,18 @@
+package com.ideal;
+
+import java.util.Scanner;
+
+/**
+ * @author lzz 578289079@qq.com
+ * @date 2018/4/25
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        int line = Integer.parseInt(args[0]);
+        for(int i=0 ;i<line;i++){
+            System.out.println("===================第"+(i+1)+"行=====================");
+        }
+
+    }
+}
