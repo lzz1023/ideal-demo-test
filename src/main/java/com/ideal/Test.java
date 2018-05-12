248918badd82a2613da938e62cb4549da2f978c4
lzz7626 <lzz@7626>
2018-05-12 13:19:52 +0800

    补充Test

--- src/main/java/com/ideal/Test.java
+++ src/main/java/com/ideal/Test.java
@@ -10,6 +10,7 @@ public class Test {
     public static void main(String[] args) {
 
         int line = Integer.parseInt(args[0]);
+        int counts = Integer.parseInt(args[1]);
         for(int i=0 ;i<line;i++){
             System.out.println("===================第"+(i+1)+"行=====================");
         }
