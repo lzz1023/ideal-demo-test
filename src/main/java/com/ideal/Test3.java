aa73101deca1570afdd0a0cb3e1a8643001ed798
lzz7626 <lzz@7626>
2018-05-12 13:18:34 +0800

    增加Test2

---
+++ src/main/java/com/ideal/Test3.java
@@ -0,0 +1,13 @@
+package com.ideal;
+
+/**
+ * @author lzz 578289079@qq.com
+ * @date 2018/5/12
+ */
+public class Test3 {
+    public static void main(String[] args) {
+
+        System.out.println("this is the test3!");
+
+    }
+}
